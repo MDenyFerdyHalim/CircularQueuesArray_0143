eec865c3bed08767f419e8d3a3c291b21f603d34
MDenyFerdyHalim <mdenyferdy@gmail.com>
2025-06-12 14:59:57 +0700

    Cek apakah antrian penuh

--- Queues.cpp
+++ Queues.cpp
@@ -6,7 +6,7 @@ class Queues
 private:
     static const int max = 5;
     int FRONT, REAR, n = 5;
-    int queues_array[5];
+    int queue_array[5];
 
 public:
     Queues()
@@ -20,7 +20,64 @@ public:
         int num;
         cout << "Enter a number: ";
         cin >> num;
-        cout << endl; 
+        cout << endl;
+
+        //1. Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; //1.a
+            return;                       //1.b
+        }
+
+        //2. Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0; // 2,b
+        }
+        else
+        {
+            // Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+        
+        // Cek jika antrian hanya memillliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // Jika elemen yang dihapus berada diposisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+
+    void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // Cek apakah antrian kosong
     }
 
 }
